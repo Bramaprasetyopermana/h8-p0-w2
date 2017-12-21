65420ee6967502089caa8f1200357af81066da69
Brama Prasetyo Permana <34509763+Bramaprasetyopermana@users.noreply.github.com>
2017-12-21 20:47:42 +0700

    Update Excercise 5.js

--- Selasa/Excercise 5.js
+++ Selasa/Excercise 5.js
@@ -1,1 +1,14 @@
+console.log('Looping pertama')
+var dope = 0;
+while (dope++ <20)
+{
+  console.log(dope +' -I love coding');
+}
+
+console.log ('Looping kedua')
+var abis = 20;
+for (abis = 20; abis >=1; abis-- )
+  {
+    console.log(abis + ' -I will Become FullStack Developer');
+  }
 
