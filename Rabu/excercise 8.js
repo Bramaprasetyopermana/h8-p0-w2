53ef348125c4620a3cd30de87abfeb01e94ea9c4
Brama Prasetyo Permana <34509763+Bramaprasetyopermana@users.noreply.github.com>
2017-12-22 21:39:47 +0700

    add excercise 8

---
+++ Rabu/excercise 8.js
@@ -0,0 +1,22 @@
+var hello = function()
+{
+  console.log('Halo Function');
+}
+  hello()
+
+var angkakali = function(num1, num2){
+return 5 * 6
+}
+console.log(angkakali(5,6))
+
+function processSentence (a,b,c,d){
+  return('nama saya ' + a + ' umur saya ' + b + 'Tahun, alamat saya di ' + c + ' dan saya punya hobby yaitu ' + d)
+}
+var nama = 'Agus';
+var age = 30;
+var address = 'Jln. Malioboro, Yogjakarta';
+var hobby = 'gaming';
+
+var fullSentence = processSentence(nama,age,address,hobby);
+
+console.log(fullSentence); 
